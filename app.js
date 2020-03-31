e55d128b2e7a7bbc3a0f102b9ba210464dd5024c
Tobi <delesafatobi@gmail.com>
2020-03-31 22:11:25 +0100

    Update app.js
    Optimising the code by removing repetitive code

--- app.js
+++ app.js
@@ -36,16 +36,45 @@ guessBtn.addEventListener('click', function() {
 
     //Check if winning number
     if (guess === winningNum) {
-        //Disable Input
-        guessInput.disabled = true;
-        //Change border color
-        guessInput.style.borderColor = 'green';
-        //Set message
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
-    } else {
+        //Game Over - Won
+        gameOver(true, `${winningNim} is correct, YOU WIN!`);
 
+    } else {
+        //Wrong number
+        guessesLeft -= 1;
+
+        if (guessesLeft === 0) {
+            //Game Over - Lost
+            gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+        } else {
+            //Game continues - Answer Wrong
+
+            //Change border color
+            guessInput.style.borderColor = 'red';
+
+            //Clear Input
+            guessInput.value = '';
+
+            //Tell user its the wrong number
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+        }
     }
 });
+
+//Game Over
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    //Disable Input
+    guessInput.disabled = true;
+    //Change border color
+    guessInput.style.borderColor = color;
+    //Set text color
+    message.style.color = color;
+    //Set message
+    setMessage(msg);
+}
 
 //Set message
 function setMessage(msg, color) {
